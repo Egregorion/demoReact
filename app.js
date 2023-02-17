5dab9d2a1112c440edefb9e4edb53d94bd6743ee
Egregorion <gregory.fourmaux@gmail.com>
2023-02-17 22:45:15 +0100

    ajout, incrémentation/décrémentation et supression ok

--- app.js
+++ app.js
@@ -16,24 +16,33 @@ const Titre4 = (props) => <h1 className="text-center">{props.content}</h1>
 
 
 const Product = (props) => 
-    <div id={props.details.id} onClick={props.handleClick}>
+    <div id={props.details.id}>
         <h2>{props.details.name}</h2>
         <p>{props.details.price}</p>
+        <button className="btn btn-primary" onClick={props.handleAdd}>Ajouter au panier</button>
     </div>
 
+const Order = (props) =>
+    <div id={props.details.id}>
+        <h2>{props.details.name}</h2>
+        <p>{props.details.price}</p>
+        <button className="btn btn-primary" onClick={props.decrement}>-</button>
+        <span>{props.details.quantity}</span>
+        <button className="btn btn-primary" onClick={props.increment}>+</button>
+    </div>
 
 //composant dans une classe
 class App extends React.Component {
     state = {
         products: [
-            {id: 1, name: "Cheeseburger", price: 5.95},
-            {id: 2, name: "CBO", price: 8.95},
-            {id: 3, name: "Filet O Fish", price:3.95}
+            {id: 1, name: "Cheeseburger", price: 5.95, quantity: 0},
+            {id: 2, name: "CBO", price: 8.95, quantity: 0},
+            {id: 3, name: "Filet O Fish", price:3.95, quantity: 0}
         ],
-        ordered : []
+        ordered: [],
     }
 
-    handleClick = (e) => {
+    handleAdd = (e) => {
         const clickedElementId = e.target.parentNode.id //renvoie la div entière 
         const clicked = this.state.products.find(element => element.id == clickedElementId)
         //this.state.ordered.push(clicked) Possible mais ne provoque pas de rerender
@@ -42,18 +51,56 @@ class App extends React.Component {
         // copiedOrdered = this.state.ordered.slice()
         //soit avec le spread operator ...
         const copiedOrdered = [...this.state.ordered]
-        //on lui ajoute notre élément
-        copiedOrdered.push(clicked)
+        //on cherche si l'élément ne se trouve pas déjà dans le tableau
+        const isOrdered = copiedOrdered.find(element => element.id == clickedElementId)
+        //Si l'élément se trouve déjà dans ordered
+        if(isOrdered){
+            //Alors on incrémente la quantité
+            isOrdered.quantity += 1
+            //Puis on met à jour le state du composant en remplaçant le contenu précédent du tableau par le contenu de la copie (qui est à jour) afin de provoquer un rerender
+            this.setState({ordered: copiedOrdered})
+        }else{
+            //Sinon, on lui ajoute notre élément
+            copiedOrdered.push(clicked)
+            //on incrémente sa quantité
+            clicked.quantity += 1
+            //On incrémente le total 
+            //const amount = clicked.quantity * clicked.price
+            //Puis on met à jour le state du composant en remplaçant le contenu précédent du tableau par le contenu de la copie (qui est à jour) afin de provoquer un rerender
+            this.setState({ordered: copiedOrdered})
+        } 
+    }
+
+    increment = (e) => {
+        const clickedElementId = e.target.parentNode.id //renvoie la div entière 
+        const copiedOrdered = [...this.state.ordered]
+        const clicked = copiedOrdered.find(element => element.id == clickedElementId)
+        clicked.quantity += 1
         this.setState({ordered: copiedOrdered})
+    } 
+
+    decrement = (e) => {
+        const clickedElementId = e.target.parentNode.id //renvoie la div entière 
+        const copiedOrdered = [...this.state.ordered]
+        const clicked = copiedOrdered.find(element => element.id == clickedElementId)
+        if(clicked.quantity > 1){
+            clicked.quantity -= 1
+            this.setState({ordered: copiedOrdered})
+        }else {
+            const clickedIndex = copiedOrdered.indexOf(clicked)
+            copiedOrdered.splice(clickedIndex, 1)
+            this.setState({ordered: copiedOrdered})
+        }  
     }
 
     render() {
         const productsList = this.state.products.map(product =>
-            <Product key={product.id} details={product} handleClick={this.handleClick} />
+            <Product key={product.id} details={product} handleAdd={this.handleAdd} />
         )
         const orderedList = this.state.ordered.map(order =>
-            <Product key={order.id} details={order} /> 
+            <Order key={order.id} details={order} increment={this.increment} decrement={this.decrement}/> 
         )
+
         return(
             <React.Fragment>
                 {productsList}
